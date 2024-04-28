cebbcffda6b151039b30fe685e18ea01adc667a6
NathanPey <jeanbobdelaforet@gmail.com>
2024-04-28 19:17:30 +0200

    finalisation

--- script.js
+++ script.js
@@ -15,6 +15,19 @@ document.querySelector('a[href="#skill"]').addEventListener('click', function(e)
         behavior: 'smooth'
     });
 });
+
+document.querySelector('a[href="#project"]').addEventListener('click', function(e) {
+    e.preventDefault(); 
+    document.querySelector('#project').scrollIntoView({
+        behavior: 'smooth' 
+    });
+});
+
+
+
+
+
+
 document.querySelector('a[href="#"]').addEventListener('click', function(e) {
     e.preventDefault(); 
     document.querySelector('#').scrollIntoView({
@@ -23,4 +36,39 @@ document.querySelector('a[href="#"]').addEventListener('click', function(e) {
 });
 
 
+
+
+document.querySelector('a[href="#contact"]').addEventListener('click', function(e) {
+    e.preventDefault(); 
+    document.querySelector('#contact').scrollIntoView({
+        behavior: 'smooth' 
+    });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const fadeElements = document.querySelectorAll('.fade-in');
+  
+    function checkFade() {
+      fadeElements.forEach(function(element) {
+        const position = element.getBoundingClientRect();
+  
+        
+        if (position.top < window.innerHeight && position.bottom >= 0) {
+   
+          if (!element.classList.contains('visible')) {
+            element.classList.add('visible');
+          }
+        } else {
+         
+          element.classList.remove('visible');
+        }
+      });
+    }
+  
+   
+    checkFade();
+    window.addEventListener('scroll', checkFade);
+  });
 
